5f5eedca28dd12a09976a723dc7e969c1a806db1
John Nye <ninjanye@gmail.com>
2014-01-17 08:56:49 +0000

    Add multiple property support

--- SearchExtensions/RankedSearchExtensions.cs
+++ SearchExtensions/RankedSearchExtensions.cs
@@ -17,6 +17,9 @@ namespace NinjaNye.SearchExtensions
         /// property contains the search term specified</returns>        
         public static IQueryable<IRanked<T>> RankedSearch<T>(this IQueryable<T> source, Expression<Func<T, string>> stringProperty, string searchTerm)
         {
+            Ensure.ArgumentNotNull(stringProperty, "stringProperty");
+            Ensure.ArgumentNotNull(searchTerm, "searchTerm");
+
             var hitCountExpression = CalculateHitCount(stringProperty, searchTerm);
             var parameterExpression = stringProperty.Parameters[0];
             var rankedInitExpression = ConstructRankedResult<T>(hitCountExpression, parameterExpression);
@@ -26,15 +29,45 @@ namespace NinjaNye.SearchExtensions
         }
 
         /// <summary>
+        /// Search multiple properties for a particular search term and return a ranked result.
+        /// </summary>
+        /// <param name="source">Source data to query</param>
+        /// <param name="searchTerm">search term to look for</param>
+        /// <param name="stringProperties">properties to search against</param>
+        /// <returns>Queryable of IRanked records where any property contains the search term</returns>
+        public static IQueryable<IRanked<T>> RankedSearch<T>(this IQueryable<T> source, string searchTerm, params Expression<Func<T, string>>[] stringProperties)
+        {
+            Ensure.ArgumentNotNull(stringProperties, "stringProperties");
+            Ensure.ArgumentNotNull(searchTerm, "searchTerm");
+
+            var singleParameter = stringProperties[0].Parameters.Single();
+            Expression combinedHitExpression = null;
+            foreach (var stringProperty in stringProperties)
+            {
+                var swappedParamExpression = SwapExpressionVisitor.Swap(stringProperty,
+                                                                        stringProperty.Parameters.Single(),
+                                                                        singleParameter);
+
+                var hitCountExpression = CalculateHitCount(swappedParamExpression, searchTerm);
+                combinedHitExpression = AddExpressions(combinedHitExpression, hitCountExpression);
+            }
+
+            var rankedInitExpression = ConstructRankedResult<T>(combinedHitExpression, singleParameter);
+            var selectExpression = Expression.Lambda<Func<T, Ranked<T>>>(rankedInitExpression, singleParameter);
+            return source.Search(searchTerm, stringProperties)
+                         .Select(selectExpression);
+        }
+
+        /// <summary>
         /// Constructs a ranked result of type T
         /// </summary>
         /// <param name="hitCountExpression">Expression representing how to calculated search hits</param>
         /// <param name="parameterExpression">property parameter</param>
         /// <returns>Expression equivalent to: new Ranked{ Hits = [hitCountExpression], Item = x }</returns>
-        private static Expression ConstructRankedResult<T>(Expression hitCountExpression, 
+        private static Expression ConstructRankedResult<T>(Expression hitCountExpression,
                                                            ParameterExpression parameterExpression)
         {
-            var rankedType = typeof (Ranked<T>);
+            var rankedType = typeof(Ranked<T>);
             var rankedCtor = Expression.New(rankedType);
             var hitProperty = rankedType.GetProperty("Hits");
             var hitValueAssignment = Expression.Bind(hitProperty, hitCountExpression);
@@ -54,15 +87,31 @@ namespace NinjaNye.SearchExtensions
             Expression searchTermExpression = Expression.Constant(searchTerm);
             Expression searchTermLengthExpression = Expression.Constant(searchTerm.Length);
             Expression emptyStringExpression = Expression.Constant("");
-            PropertyInfo stringLengthProperty = typeof (string).GetProperty("Length");
+            PropertyInfo stringLengthProperty = typeof(string).GetProperty("Length");
             var lengthExpression = Expression.Property(stringProperty.Body, stringLengthProperty);
-            MethodInfo replaceMethod = typeof (string).GetMethod("Replace", new[] {typeof (string), typeof (string)});
-            var replaceExpression = Expression.Call(stringProperty.Body, replaceMethod, 
+            MethodInfo replaceMethod = typeof(string).GetMethod("Replace", new[] { typeof(string), typeof(string) });
+            var replaceExpression = Expression.Call(stringProperty.Body, replaceMethod,
                                                     searchTermExpression, emptyStringExpression);
             var replacedLengthExpression = Expression.Property(replaceExpression, stringLengthProperty);
             var characterDiffExpression = Expression.Subtract(lengthExpression, replacedLengthExpression);
             var hitCountExpression = Expression.Divide(characterDiffExpression, searchTermLengthExpression);
             return hitCountExpression;
+        }
+
+        /// <summary>
+        /// Join two expressions using the add operation
+        /// </summary>
+        /// <param name="existingExpression">Expression being summed</param>
+        /// <param name="expressionToAdd">Expression to append</param>
+        /// <returns>New expression containing both expressions joined using the Add operation</returns>
+        private static Expression AddExpressions(Expression existingExpression, Expression expressionToAdd)
+        {
+            if (existingExpression == null)
+            {
+                return expressionToAdd;
+            }
+
+            return Expression.Add(existingExpression, expressionToAdd);
         }
     }
 }
