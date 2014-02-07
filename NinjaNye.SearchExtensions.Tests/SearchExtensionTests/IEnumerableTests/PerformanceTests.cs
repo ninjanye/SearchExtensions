d0b65761850b3b4f6a9b1f9ab76acac4a308b372
John Nye <ninjanye@gmail.com>
2014-02-07 08:37:01 +0000

    Ad Search test

--- NinjaNye.SearchExtensions.Tests/SearchExtensionTests/IEnumerableTests/PerformanceTests.cs
+++ NinjaNye.SearchExtensions.Tests/SearchExtensionTests/IEnumerableTests/PerformanceTests.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
-using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace NinjaNye.SearchExtensions.Tests.SearchExtensionTests.IEnumerableTests
 {
@@ -13,15 +12,15 @@ namespace NinjaNye.SearchExtensions.Tests.SearchExtensionTests.IEnumerableTests
     [TestFixture]
     public class PerformanceTests
     {
-        private List<string> enumerableData = new List<string>();
+        private readonly List<string> enumerableData = new List<string>();
             
         [TestFixtureSetUp]
         public void ClassSetup()
         {
-            var random = new Random();
             for (int i = 0; i < 1000000; i++)
             {
-                enumerableData.Add(random.Next(1,1000).ToString());
+                Guid guid = Guid.NewGuid();
+                enumerableData.Add(guid.ToString());
             }
         }
 
@@ -32,12 +31,36 @@ namespace NinjaNye.SearchExtensions.Tests.SearchExtensionTests.IEnumerableTests
             var stopwatch = Stopwatch.StartNew();
 
             //Act
-            var result = enumerableData.Search(s => s, "12", StringComparison.OrdinalIgnoreCase).ToList();
+            var result = enumerableData.Search("abc", s => s, StringComparison.OrdinalIgnoreCase).ToList();
 
             //Assert
             stopwatch.Stop();
             Console.WriteLine("Time taken: {0}", stopwatch.Elapsed);
+            Console.WriteLine("Found: " + result.Count);
             Assert.IsTrue(1 > stopwatch.Elapsed.Seconds);
+        }
+
+        [Test]
+        public void SearchAMillionRecords_SearchShouldBeWithin20pcOfEquivalentLambdaSearch()
+        {
+            //Arrange
+            var stopwatch = Stopwatch.StartNew();
+
+            //Act
+            var lambdaResult = enumerableData.Where(s => s.IndexOf("abc", StringComparison.OrdinalIgnoreCase) > -1).ToList();
+            stopwatch.Stop();
+            var lambdaTime = stopwatch.Elapsed;
+            stopwatch.Restart();
+            var result = enumerableData.Search("abc", s => s, StringComparison.OrdinalIgnoreCase).ToList();
+            stopwatch.Stop();   
+            var searchTime = stopwatch.Elapsed;
+
+            //Assert
+            Console.WriteLine("Lambda time taken: {0}", lambdaTime);
+            Console.WriteLine("Search time taken: {0}", searchTime);
+            double threshold = lambdaTime.Ticks + (lambdaTime.Ticks*0.15);
+            Console.WriteLine("Time threshhold: {0}", new TimeSpan((int) threshold));
+            Assert.LessOrEqual(searchTime.Ticks, threshold);
         }
     }
 }
---
+++ NinjaNye.SearchExtensions.Tests/SearchExtensionTests/IEnumerableTests/SearchTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using NUnit.Framework;
+
+namespace NinjaNye.SearchExtensions.Tests.SearchExtensionTests.IEnumerableTests
+{
+    [TestFixture]
+    public class SearchTests
+    {
+        private readonly List<string> enumerableData = new List<string>();
+
+        [TestFixtureSetUp]
+        public void ClassSetup()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Guid guid = Guid.NewGuid();
+                enumerableData.Add(guid.ToString());
+            }
+        }
+
+        [Test]
+        public void Search_SearchTermNotSupplied_AllDataReturned()
+        {
+            //Arrange
+            
+            //Act
+            var result = enumerableData.Search((string)null, s => s);
+
+            //Assert
+            Assert.AreEqual(enumerableData, result);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Search_SearchPropertyNotSupplied_ThrowsArgumentNullException()
+        {
+            //Arrange
+            
+            //Act
+            enumerableData.Search("test", (Expression<Func<string, string>>)null);
+
+            //Assert
+            Assert.Fail("Expected an Argument Null expception to occur");
+        }
+
+        [Test]
+        public void Search_SearchAllProperties_SearchesAllProperties()
+        {
+            //Arrange
+            var data = new List<TestClass>();
+            data.Add(new TestClass{Name = "abcd", Description = "efgh"});
+            data.Add(new TestClass{Name = "ijkl", Description = "mnop"});
+            data.Add(new TestClass{Name = "qrst", Description = "uvwx"});
+            data.Add(new TestClass{Name = "yz", Description = "abcd"});
+            
+            //Act
+            var result = data.AsQueryable().Search("b");
+
+            //Assert
+            Assert.AreEqual(2, result.Count());
+
+        }
+
+        private class TestClass
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+
+    }
+}
