78aadaa116c15790461f884f587991990e596448
John Nye <ninjanye@gmail.com>
2014-11-24 09:22:07 +0000

    Test Levenshtein Distance against database context

---
+++ NinjaNye.SearchExtensions.Tests.Integration/Fluent/FluentLevenshteinTests.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NinjaNye.SearchExtensions.Levenshtein;
+
+namespace NinjaNye.SearchExtensions.Tests.Integration.Fluent
+{
+    [TestFixture]
+    public class FluentLevenshteinTests : IDisposable
+    {
+        private readonly TestContext context = new TestContext();
+
+        [Test]
+        public void LevenshteinDistanceOf_GetDistanceToString_ReturnAllRecords()
+        {
+            //Arrange
+            int totalRecords = context.TestModels.Count();
+
+            //Act
+            var result = context.TestModels.LevenshteinDistanceOf(x => x.StringOne).ComparedTo("test");
+
+            //Assert
+            Assert.AreEqual(totalRecords, result.Count());
+        }
+
+        [Test]
+        public void LevenshteinDistanceOf_GetDistanceToString_DistancesAreCorrect()
+        {
+            //Arrange
+
+            //Act
+            var result = context.TestModels.LevenshteinDistanceOf(x => x.StringOne).ComparedTo("test");
+
+            //Assert
+            Assert.IsTrue(result.All(x => x.Distance == LevenshteinProcessor.LevenshteinDistance(x.Item.StringOne, "test")));
+        }
+
+        [Test]
+        public void LevenshteinDistanceOf_GetDistanceToProperty_ReturnAllRecords()
+        {
+            //Arrange
+            int totalRecords = context.TestModels.Count();
+
+            //Act
+            var result = context.TestModels.LevenshteinDistanceOf(x => x.StringOne).ComparedTo(x => x.StringTwo);
+
+            //Assert
+            Assert.AreEqual(totalRecords, result.Count());
+        }
+
+        [Test]
+        public void LevenshteinDistanceOf_GetDistanceToProperty_DistancesAreCorrect()
+        {
+            //Arrange
+
+            //Act
+            var result = context.TestModels.LevenshteinDistanceOf(x => x.StringOne).ComparedTo(x => x.StringTwo);
+
+            //Assert
+            Assert.IsTrue(result.All(x => x.Distance == LevenshteinProcessor.LevenshteinDistance(x.Item.StringOne, x.Item.StringTwo)));
+        }
+
+        public void Dispose()
+        {
+            this.context.Dispose();
+        }
+    }
+}
