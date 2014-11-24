443230efb20752125904ad70a2d4599b91c4853a
John Nye <ninjanye@gmail.com>
2014-11-24 14:10:09 +0000

    UPdate assembly version and release notes

--- NinjaNye.SearchExtensions/Properties/AssemblyInfo.cs
+++ NinjaNye.SearchExtensions/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("John Nye")]
 [assembly: AssemblyProduct("NinjaNye.SearchExtensions")]
-[assembly: AssemblyCopyright("Copyright © 2013")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.*")]
-[assembly: AssemblyFileVersion("1.2")]
+[assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyFileVersion("1.3")]
