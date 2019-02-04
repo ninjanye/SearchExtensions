13ea5b91ce47eb898c0e1d716b39b101726a9908
John Nye <John.Nye@decision.tech>
2019-02-04 08:57:33 +0000

    Create nuget specs for Soundex and Levenshtein packages

--- BuildNugetPackage.ps1
+++ BuildNugetPackage.ps1
@@ -1,2 +1,8 @@
 cd .\NinjaNye.SearchExtensions\
 dotnet pack -c Release -o .
+
+cd .\NinjaNye.SearchExtensions.Levenshtein\
+dotnet pack -c Release -o .
+
+cd .\NinjaNye.SearchExtensions.Soundex\
+dotnet pack -c Release -o .
